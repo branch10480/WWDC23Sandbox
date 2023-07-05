1d1e170083f12d2cccbaebb82672571ba2e2052c
branch10480 <toshiharu.imaeda@gmail.com>
2023-07-05 21:03:33 +0900

    Update

--- Sources/AppFeature/SFSymbolAnimation/SymbolView.swift
+++ Sources/AppFeature/SFSymbolAnimation/SymbolView.swift
@@ -10,25 +10,82 @@ import SwiftUI
 struct SymbolView: View {
     @State var isActive = false
     @State var bounceValue: Int = 0
+    @State var isPaused = false
+    @State var isMoonAppeared = true
+    @State var wifiSignalLevel = 0.0
 
     var body: some View {
         VStack(spacing: 32) {
             Image(systemName: "wifi.router")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 100, alignment: .center)
+                .frame(width: 50, alignment: .center)
                 .symbolEffect(.variableColor.iterative.reversing, isActive: isActive)
-
+            
             VStack {
                 Image(systemName: "antenna.radiowaves.left.and.right")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 100, alignment: .center)
-//                    .symbolEffect(.bounce, options: .repeat(2), value: bounceValue)
-                    .symbolEffect(.variableColor.iterative, options: .repeat(2), value: bounceValue)
+                    .frame(width: 50, alignment: .center)
+                    .symbolEffect(.bounce, options: .repeat(2), value: bounceValue)
+//                    .symbolEffect(.variableColor.iterative, options: .repeat(2), value: bounceValue)
+                    .onAppear {
+                        var transaction = Transaction()
+                        transaction.disablesAnimations = true
+                        withTransaction(transaction) {
+                            bounceValue = 1
+                        }
+                    }
 
                 Button("Animate") {
                     bounceValue += 1
+                }
+            }
+            
+            VStack {
+                Image(systemName: isPaused ? "pause.fill" : "play.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50)
+                    .contentTransition(.symbolEffect(.replace.offUp))
+
+                Button("Tap!") {
+                    isPaused = !isPaused
+                }
+            }
+
+            VStack {
+                HStack(spacing: 16) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 50, height: 50)
+
+                    if isMoonAppeared {
+                        Image(systemName: "moon.stars")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50)
+//                            .symbolEffect(.disappear, isActive: !isMoonAppeared)
+                            .transition(.symbolEffect(.automatic))
+                    }
+
+                    Circle()
+                        .frame(width: 50, height: 50)
+                }
+
+                Button("Toggle Moon") {
+                    isMoonAppeared = !isMoonAppeared
+                }
+            }
+
+            VStack {
+                Image(systemName: "wifi", variableValue: wifiSignalLevel)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50)
+
+                Button("Change Wifi Signal Level") {
+                    let newValue = wifiSignalLevel == 1.0 ? 0 : wifiSignalLevel + 0.2
+                    wifiSignalLevel = newValue
                 }
             }
         }
